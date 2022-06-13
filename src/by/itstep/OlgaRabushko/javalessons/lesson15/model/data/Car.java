45a345e0f5c9e01525bbc938f28f30996bb9bd2b
Gigabyte <Olga-Rabushko1983>
2022-06-14 02:36:10 +0300

    Add Car, Manager and Main classes

---
+++ src/by/itstep/OlgaRabushko/javalessons/lesson15/model/data/Car.java
@@ -0,0 +1,6 @@
+package by.itstep.OlgaRabushko.javalessons.lesson15.model.data;
+
+public class Car {
+    public String name;
+    public int price;
+}
---
+++ src/by/itstep/OlgaRabushko/javalessons/lesson15/view/ConsolePrinter.java
@@ -0,0 +1,8 @@
+package by.itstep.OlgaRabushko.javalessons.lesson15.view;
+
+public class ConsolePrinter {
+    public void print(String msg) {
+    System.out.print(msg);
+}
+}
+
